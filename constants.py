6165e314d237b0cd36fa3e66b36956c378a3c678
donbasta <faddilafarras@gmail.com>
2021-06-29 16:49:52 +0700

    add constants

---
+++ constants.py
@@ -0,0 +1,10 @@
+FACE_DIM = 224
+NUM_MAX = 10000000
+ESC_KEY = 27
+
+VIDEOS_FOLDER = "./data/videos"
+PROCESSED_FACE_FOLDER = "./data/processed_face"
+RAW_IMAGE_FOLDER = "./data/raw_face"
+
+HAARCASCADE_FRONTALFACE_ALT = ".\\models\\pretrained\\haarcascade_frontalface_alt.xml"
+HAARCASCADE_FRONTALFACE_DEFAULT = ".\\models\\pretrained\\haarcascade_frontalface_default.xml"
