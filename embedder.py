b8501526f83771ad98bc0bdfd7b16eb43948531f
donbasta <faddilafarras@gmail.com>
2021-06-29 16:50:00 +0700

    udpate embedder

--- embedder.py
+++ embedder.py
@@ -1,34 +1,68 @@
 from keras_vggface.vggface import VGGFace
 from keras_vggface.utils import preprocess_input
-from keras_vggface.utils import decode_predictions
+from keras.preprocessing import image
 
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import pickle
+import glob
 
-vgg_senet50_model = VGGFace(model='senet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
+from constants import PROCESSED_FACE_FOLDER
 
-def get_embeddings(folder_faces, save=True, outdir='default'):
+def picklify(filename, stuff):
+    save_stuff = open(filename, "wb")
+    pickle.dump(stuff, save_stuff)
+    save_stuff.close()
+
+class Embedder(object):
+    def __init__(self, folder_path, outdir="./data/embeddings/tes2.pickle"):
+        self.model = VGGFace(model='senet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
+        self.folder = folder_path
+        self.folders = list(glob.iglob(os.path.join(self.folder, '*')))
+        self.names = [os.path.basename(folder) for folder in self.folders]
+        self.outdir = outdir
     
-    faces = [plt.imread(os.path.join(folder_faces, facepath)) for facepath in os.listdir(folder_faces)]
+    def image2x(self, image_path):
+        img = image.load_img(image_path, target_size=(224, 224))
+        pixels = image.img_to_array(img)
+        pixels = np.expand_dims(pixels, axis=0)
+        pixels = preprocess_input(pixels, version=2)  # or version=2
+        return pixels
 
-    # faces = []
-    # for facepath in os.listdir(folder_faces):
-    #     print("facepath: ", facepath)
-    #     path = os.path.join(folder_faces, facepath)
-    #     faces.append(plt.imread(path))
-    #     plt.imshow(plt.imread(path))
-    #     plt.show()
+    def cal_mean_feature(self, image_folder):
+        face_images = list(glob.iglob(os.path.join(image_folder, '*.*')))
 
-    samples = np.asarray(faces, 'float32')
-    samples = preprocess_input(samples, version=2)
-    features = vgg_senet50_model.predict(samples)
-    if save:
-        np.save(f'{outdir}.npy', features)
-    return features
+        def chunks(l, n):
+            for i in range(0, len(l), n):
+                yield l[i:min(i + n, len(l) - 1)]
+
+        batch_size = 32
+        face_images_chunks = chunks(face_images, batch_size)
+        fvecs = None
+        for face_images_chunk in face_images_chunks:
+            images = np.concatenate([self.image2x(face_image) for face_image in face_images_chunk])
+            batch_fvecs = self.model.predict(images)
+            if fvecs is None:
+                fvecs = batch_fvecs
+            else:
+                fvecs = np.append(fvecs, batch_fvecs, axis=0)
+        return np.array(fvecs).sum(axis=0) / len(fvecs)
+
+    def get_embeddings(self, save=True, outdir='default'):
+        embeddings = []
+        for i, folder in enumerate(self.folders):
+            name = self.names[i]
+            print(f"processing {name}.....")
+            img_folder = os.path.join(PROCESSED_FACE_FOLDER, name)
+            mean_features = self.cal_mean_feature(image_folder=img_folder)
+            embeddings.append({
+                "name": name,
+                "features": mean_features
+            })
+        picklify(self.outdir, embeddings)
 
 if __name__ == "__main__":
-    folder_faces = input("enter path of the face images: ")
-    outdir = input("enter path for the face embeddings: ")
-    outdir = f'data/embeddings/{outdir}'
-    embeddings = get_embeddings(folder_faces, outdir=outdir)
+    embedder = Embedder(PROCESSED_FACE_FOLDER)
+    embedder.get_embeddings()
